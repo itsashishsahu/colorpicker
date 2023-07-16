ca70c0d57ef8a7d5b4f6abf3716873906fb78496
Ashish Sahu <itsashishsahu.inc@gmail.com>
2023-07-17 01:27:51 +0530

    completed

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelector("#white").addEventListener("click", function(){
+  document.body.style.background = 'white';
+});
+document.querySelector("#red").addEventListener("click", function(){
+  document.body.style.background = 'red';
+});
+document.querySelector("#green").addEventListener("click", function(){
+  document.body.style.background = 'green';
+});
+document.querySelector("#blue").addEventListener("click", function(){
+  document.body.style.background = 'blue';
+});
+document.querySelector("#black").addEventListener("click", function(){
+  document.body.style.background = 'black';
+});
+document.querySelector("#yellow").addEventListener("click", function(){
+  document.body.style.background = 'yellow';
+});
+
